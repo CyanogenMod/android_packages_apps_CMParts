7b26cbb8a13eea0bc9a9e9f8a2237ab18fcaadb3
elektroschmock <elektroschmock78@googlemail.com>
2011-10-24 19:25:43 +0200

    Revert "(2/2) Add status bar battery icon configuration."
    This reverts commit 20626da1f4e4fb818689383c24eab0f9ac72c98b.
    
    Conflicts:
    
    	res/values-de/strings.xml
    	res/values/arrays.xml
    	res/values/strings.xml
    	res/xml/ui_status_bar.xml
    	src/com/cyanogenmod/cmparts/activities/UIStatusBarActivity.java

--- src/com/cyanogenmod/cmparts/activities/UIStatusBarActivity.java
+++ src/com/cyanogenmod/cmparts/activities/UIStatusBarActivity.java
@@ -36,13 +36,13 @@ public class UIStatusBarActivity extends PreferenceActivity implements OnPrefere
 
     private static final String PREF_STATUS_BAR_AM_PM = "pref_status_bar_am_pm";
 
-    private static final String PREF_STATUS_BAR_BATTERY = "pref_status_bar_battery";
-
     private static final String PREF_STATUS_BAR_CLOCK = "pref_status_bar_clock";
 
     private static final String PREF_STATUS_BAR_ONEPERC_BATTERY = "pref_status_bar_oneperc_battery";
 
     private static final String PREF_STATUS_BAR_CM_BATTERY_COLOR = "pref_status_bar_cm_battery_color";
+
+    private static final String PREF_STATUS_BAR_CM_BATTERY = "pref_status_bar_cm_battery";
 
     private static final String PREF_STATUS_BAR_COMPACT_CARRIER = "pref_status_bar_compact_carrier";
 
@@ -60,8 +60,6 @@ public class UIStatusBarActivity extends PreferenceActivity implements OnPrefere
     private static final String PREF_STATUS_BAR_HEADSET = "pref_status_bar_headset";
 
     private ListPreference mStatusBarAmPm;
-
-    private ListPreference mStatusBarBattery;
 
     private ListPreference mStatusBarCmSignal;
 
@@ -102,6 +100,8 @@ public class UIStatusBarActivity extends PreferenceActivity implements OnPrefere
         mStatusBarForcePlmnDisplay = (CheckBoxPreference) prefSet.findPreference(PREF_FORCE_PLMN_DISPLAY);
         mStatusBarCmBatteryColor = (ListPreference) prefSet
                 .findPreference(PREF_STATUS_BAR_CM_BATTERY_COLOR);
+        mStatusBarCmBattery = (CheckBoxPreference) prefSet
+                .findPreference(PREF_STATUS_BAR_CM_BATTERY);
         mStatusBarBrightnessControl = (CheckBoxPreference) prefSet
                 .findPreference(PREF_STATUS_BAR_BRIGHTNESS_CONTROL);
         mStatusBarHeadset = (CheckBoxPreference) prefSet
@@ -109,6 +109,8 @@ public class UIStatusBarActivity extends PreferenceActivity implements OnPrefere
 
         mStatusBarClock.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_CLOCK, 1) == 1));
+        mStatusBarCmBattery.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CM_BATTERY, 0) == 1));
         mStatusBarCompactCarrier.setChecked((Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_COMPACT_CARRIER, 0) == 1));
         mStatusBarForcePlmnDisplay.setChecked((Settings.System.getInt(getContentResolver(),
@@ -128,18 +130,12 @@ public class UIStatusBarActivity extends PreferenceActivity implements OnPrefere
         }
 
         mStatusBarAmPm = (ListPreference) prefSet.findPreference(PREF_STATUS_BAR_AM_PM);
-        mStatusBarBattery = (ListPreference) prefSet.findPreference(PREF_STATUS_BAR_BATTERY);
         mStatusBarCmSignal = (ListPreference) prefSet.findPreference(PREF_STATUS_BAR_CM_SIGNAL);
 
         int statusBarAmPm = Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_AM_PM, 2);
         mStatusBarAmPm.setValue(String.valueOf(statusBarAmPm));
         mStatusBarAmPm.setOnPreferenceChangeListener(this);
-
-        int statusBarBattery = Settings.System.getInt(getContentResolver(),
-                Settings.System.STATUS_BAR_BATTERY, 0);
-        mStatusBarBattery.setValue(String.valueOf(statusBarBattery));
-        mStatusBarBattery.setOnPreferenceChangeListener(this);
 
         int signalStyle = Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_CM_SIGNAL_TEXT, 0);
@@ -162,11 +158,6 @@ public class UIStatusBarActivity extends PreferenceActivity implements OnPrefere
             int statusBarAmPm = Integer.valueOf((String) newValue);
             Settings.System.putInt(getContentResolver(), Settings.System.STATUS_BAR_AM_PM,
                     statusBarAmPm);
-            return true;
-        } else if (preference == mStatusBarBattery) {
-            int statusBarBattery = Integer.valueOf((String) newValue);
-            Settings.System.putInt(getContentResolver(), Settings.System.STATUS_BAR_BATTERY,
-                    statusBarBattery);
             return true;
         } else if (preference == mStatusBarCmSignal) {
             int signalStyle = Integer.valueOf((String) newValue);
